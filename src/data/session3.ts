e1311f7d47814f8bf1b608baaeb471be30f8e496
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2024-07-17 18:25:24 +0800

    Update session3.ts

--- src/data/session3.ts
+++ src/data/session3.ts
@@ -9,11 +9,53 @@ const session: SessionItem = {
 
 const parts: PartModel[] = [
     {
-        outline: "7",
-        speaker: "Mark Lou Mejias",
-        start: "01:50",
-        time: 13,
-        title: "Believe the Truth About Jesus (Symposium)​—The Word"
+        outline: "20",
+        speaker: "Jepte Famat",
+        start: "00:00",
+        time: 11,
+        title: "Messianic Prophecy Fulfilled! (Symposium)​—Preceded by a Messenger"
+    },
+    {
+        outline: "21",
+        speaker: "Keno Dagalea",
+        start: "00:00",
+        time: 9,
+        title: "Messianic Prophecy Fulfilled! (Symposium)​—Born to a Virgin"
+    },
+    {
+        outline: "22",
+        speaker: "Benjamin Dela Cruz",
+        start: "00:00",
+        time: 9,
+        title: "Messianic Prophecy Fulfilled! (Symposium)—Born in Bethlehem"
+    },
+    {
+        outline: "23",
+        speaker: "David Dinawanao",
+        start: "00:00",
+        time: 9,
+        title: "Messianic Prophecy Fulfilled! (Symposium)—Protected as a Child"
+    },
+    {
+        outline: "24",
+        speaker: "Samuel Dela Cruz",
+        start: "00:00",
+        time: 9,
+        title: "Messianic Prophecy Fulfilled! (Symposium)—Called a Nazarene"
+    },
+    {
+        outline: "25",
+        speaker: "Michael Waniwan",
+        start: "00:00",
+        time: 15,
+        title: "Messianic Prophecy Fulfilled! (Symposium)—Arrived at the Appointed Time"
+    },
+    {
+        outline: "26",
+        speaker: "Rolly Santa Maria",
+        start: "00:00",
+        time: 29,
+        title: "Baptism: Continue to Be “Submissive to the Good News”"
     }
 ]
 
