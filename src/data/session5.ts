27d88e1ec36cef38fe7a58acd4d5a452aa81b4c7
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2024-07-17 18:13:18 +0800

    Update session5.ts

--- src/data/session5.ts
+++ src/data/session5.ts
@@ -9,11 +9,60 @@ const session: SessionItem = {
 
 const parts: PartModel[] = [
     {
-        outline: "7",
-        speaker: "Mark Lou Mejias",
-        start: "01:50",
+        outline: "38",
+        speaker: "",
+        start: "00:00",
+        time: 14,
+        title: "Imitate Those Who Had Faith in the Good News (Symposium)​—Zechariah"
+    },
+    {
+        outline: "39",
+        speaker: "",
+        start: "00:00",
         time: 13,
-        title: "Believe the Truth About Jesus (Symposium)​—The Word"
+        title: "Imitate Those Who Had Faith in the Good News (Symposium)—Elizabeth"
+    },
+    {
+        outline: "40",
+        speaker: "",
+        start: "00:00",
+        time: 13,
+        title: "Imitate Those Who Had Faith in the Good News (Symposium)—Mary"
+    },
+    {
+        outline: "41",
+        speaker: "",
+        start: "00:00",
+        time: 13,
+        title: "Imitate Those Who Had Faith in the Good News (Symposium)—Joseph"
+    },
+    {
+        outline: "42",
+        speaker: "",
+        start: "00:00",
+        time: 13,
+        title: "Imitate Those Who Had Faith in the Good News (Symposium)—Simeon and Anna"
+    },
+    {
+        outline: "43",
+        speaker: "",
+        start: "00:00",
+        time: 15,
+        title: "Imitate Those Who Had Faith in the Good News (Symposium)—Jesus"
+    },
+    {
+        outline: "44",
+        speaker: "",
+        start: "00:00",
+        time: 29,
+        title: "Public Bible Discourse: Why We Don’t Fear Bad News"
+    },
+    {
+        outline: "45",
+        speaker: "",
+        start: "00:00",
+        time: 29,
+        title: "Summary of The Watchtower"
     }
 ]
 
