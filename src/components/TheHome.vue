a0a3bbe5c5faa74d57f02c3c013020979a6bb0b4
Jezreel Tan <jezdtanslc90@gmail.com>
2024-11-16 12:18:34 +0800

    formatting

--- src/components/TheHome.vue
+++ src/components/TheHome.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="bg-black/90">
         <div class="p-5 md:pt-12 md:px-24 h-screen max-w-5xl min-w-3xl m-auto shadow-xl shadow-white/5">
-            <div>
-                <h1 class="text-2xl md:text-4xl font-bold text-white">“Behave in a Manner Worthy of the Good News”</h1>
+            <div class="pt-8">
+                <h1 class="text-2xl md:text-4xl font-bold text-white py-3">“Behave in a Manner Worthy of the Good News”</h1>
                 <h2 class="text-md md:text-2xl text-gray-600">2024-2025 Circuit Assembly Program With Circuit Overseer
                 </h2>
             </div>
